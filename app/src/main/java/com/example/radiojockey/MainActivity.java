7437928a326dfe6e9d48bde759a251a9eb637dd3
Saksham Seth <sakshamseth@Sakshams-MacBook-Pro.local>
2020-07-28 15:02:06 +0530

    Iteration 1

--- app/src/main/java/com/example/radiojockey/MainActivity.java
+++ app/src/main/java/com/example/radiojockey/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.radiojockey;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private String userId;
     private DatabaseReference mRef;
+    private StorageReference storageRef;
     EditText occasion;
     EditText name;
     EditText youtubeLink;
@@ -36,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
         user =  mAuth.getCurrentUser();
         userId = user.getUid();
         mRef = mDatabase.child("Data").child(userId);
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+
         occasion = (EditText)findViewById(R.id.occasion);
         name = (EditText)findViewById(R.id.Name);
         youtubeLink = (EditText) findViewById(R.id.youtubeLink);
@@ -59,7 +71,9 @@ public class MainActivity extends AppCompatActivity {
                 mRef.child("occasion").setValue(occasion_string);
                 mRef.child("name").setValue(name_string);
                 mRef.child("youtube_link").setValue(youtubeLink_string);
-                //mRef.child("Audio").setValue(uri);
+                mRef.child("Audio_uri").setValue(uri.toString());
+                String path = name_string + "/" + occasion_string + "/" + youtubeLink_string + "/" + uri.toString();
+                uploadData(uri,path);
 
             }
         });
@@ -76,5 +90,29 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadData(Uri videoUri, String path) {
+        if(videoUri != null){
+            StorageReference videoRef = storageRef.child(path);
+            UploadTask uploadTask = videoRef.putFile(videoUri);
+
+            uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful())
+                        Toast.makeText(MainActivity.this, "Upload Complete", Toast.LENGTH_SHORT).show();
+                    //progressBarUpload.setVisibility(View.INVISIBLE);
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    //updateProgress(taskSnapshot);
+                }
+            });
+        }else {
+            Toast.makeText(MainActivity.this, "Nothing to upload", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
